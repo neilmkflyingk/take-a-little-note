3e02f79c1cfa510d6508f4708ffd541f06fb323d
neilmkflyingk <126199320+neilmkflyingk@users.noreply.github.com>
2023-08-21 13:48:54 -0700

    debug

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 // express creates a route for every file in the 'public' folder
 app.use(express.static('public'));
+
 // express to handle data parse and middle wear created req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
